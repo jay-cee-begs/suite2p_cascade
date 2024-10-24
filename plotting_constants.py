63a890ac6a0e5ebe4cc5f2d6b4071302d457fe14
JDKleber <justus.donner@gmail.com>
2024-10-24 10:15:42 +0200

    Add dictionaries & function parameters to configurations

--- plotting_constants.py
+++ plotting_constants.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from configurations import *
-from configurations import main_folder
+
+from configurations import main_folder, TimePoints, Groups22, pairs, parameters
 from functions_plots import *
 from statannotations.Annotator import Annotator
 from jd_plot_functions import ez_sign_plot, load_and_adjust
@@ -8,41 +8,30 @@ from jd_plot_functions import ez_sign_plot, load_and_adjust
 
 
 ##Dictionaries and lists
-TimePoints = {
-    'ACS' : 'ACSF',
-    'NBA': 'NBA',
-    'NaO': 'NaOH'
-}
+# TimePoints = {
+#     'ACS' : 'ACSF',
+#     'NBA': 'NBA',
+#     'NaO': 'NaOH'
+# }
 
-Groups22 ={
-    'ACSF_baseline' : 'Baseline',
-    'ACSF_GBZ' : 'GBZ',
-    'ACSF_TTX' : 'TTX',
-    'NBA_baseline' : 'Baseline',
-    'NBA_GBZ' : 'GBZ',
-    'NBA_TTX' : 'TTX',
-    'NaOH_baseline' : 'Baseline',
-    'NaOH_GBZ' : 'GBZ',
-    'NaOH_TTX' : 'TTX'
-    }
+# Groups22 ={
+#     'ACSF_baseline' : 'Baseline',
+#     'ACSF_GBZ' : 'GBZ',
+#     'ACSF_TTX' : 'TTX',
+#     'NBA_baseline' : 'Baseline',
+#     'NBA_GBZ' : 'GBZ',
+#     'NBA_TTX' : 'TTX',
+#     'NaOH_baseline' : 'Baseline',
+#     'NaOH_GBZ' : 'GBZ',
+#     'NaOH_TTX' : 'TTX'
+#     }
 
 #filepath = main_folder + r'\new_experiment_summary.csv'
-pairs = [('Baseline', 'GBZ'), ('Baseline', 'TTX')]
+# pairs = [('Baseline', 'GBZ'), ('Baseline', 'TTX')]
 
 
 
 df2 = load_and_adjust(TimePoints, Groups22) #run once but only once since it cant edit the already edited df
-s1 = ez_sign_plot(df = df2, 
-            x= "Group", 
-            feature = "Total_Estimated_Spikes", 
-            type = 'box', 
-            plotby ='Time_Point', 
-            y_label = "y label", 
-            x_label = '', 
-            stat_test='Mann-Whitney', 
-            legend='auto', 
-            location='inside', 
-            palette = 'viridis', 
-            testby = pairs )  # Added pairs here
+s1 = ez_sign_plot(df=df2, **parameters)  # Added pairs here
 
 plt.show(s1)
