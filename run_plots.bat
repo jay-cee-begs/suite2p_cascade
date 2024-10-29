6f18d81b12dccc682b72eac430c85fdd83f6edc7
JDKleber <justus.donner@gmail.com>
2024-10-29 14:09:30 +0100

    Split sequential_.bat into 2 seperate files

---
+++ run_plots.bat
@@ -0,0 +1,8 @@
+:: Activate the third virtual environment
+CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" data_env
+
+:: Run the third script 
+python C:\Users\Justus\calcium\data_data.py
+
+:: keep terminal open 
+pause
---
+++ run_s2p_cascade.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Activate the first virtual environment, evt. use Activate.ps1
+
+CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" suite2p
+
+:: Run the first script
+python C:\Users\Justus\calcium\data_suite2p.py
+
+:: Deactivate the first environment
+CALL conda deactivate
+
+:: Activate the second virtual environment
+CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" cascade
+
+:: Run the second script
+python C:\Users\Justus\calcium\data_cascade.py
+
+:: Deactivate the second environment
+CALL conda deactivate
+
