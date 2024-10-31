03dbe9b35e4703288c27092c5d35bf52d53770cf
JDKleber <justus.donner@gmail.com>
2024-10-31 12:45:35 +0100

    Edit .bat file to call s2p

--- run_s2p_gui.bat
+++ run_s2p_gui.bat
@@ -3,7 +3,7 @@
 :: Activate the s2p environment
 CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" suite2p
 
-python C:\Users\Justus\Documents\GitHub\suite_cascade1p_ju\jd_s2p_gui.py
+python -m suite2p
 
 :: Deactivate the environment
 CALL conda deactivate
