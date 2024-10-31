995e863c74c94b0f2feb8b5a405adbade82fae17
JDKleber <justus.donner@gmail.com>
2024-10-31 16:04:05 +0100

    Change paths to accommodate for new names

--- run_plots.bat
+++ run_plots.bat
@@ -1,8 +1,10 @@
-:: Activate the third virtual environment
+@echo off
+
+:: Activate the data_env virtual environment
 CALL "C:\Users\Justus\Anaconda3\Scripts\activate.bat" data_env
 
-:: Run the third script 
-python C:\Users\Justus\calcium\data_data.py
+:: Run the plotting script 
+python C:\Users\Justus\Documents\GitHub\suite_cascade1p_ju\plotting_constants.py
 
 :: keep terminal open 
 pause
