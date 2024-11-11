611c58fea07590cddb5eb308ba4719971ca4d977
JDKleber <justus.donner@gmail.com>
2024-11-11 14:29:55 +0100

    Comment out unused import

--- plotting_constants.py
+++ plotting_constants.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import gui_configurations
-from gui_configurations import main_folder, parameters, TimePoints, Groups22
-from functions_plots import *
+from gui_configurations import main_folder, parameters, TimePoints, Groups22 
+#from functions_plots import *
 from statannotations.Annotator import Annotator
 from jd_plot_functions import ez_sign_plot, load_and_adjust
 import importlib # to reload the gui_configurations file
@@ -13,8 +13,9 @@ parameters = gui_configurations.parameters
 
 
 df2 = load_and_adjust(TimePoints, Groups22) #run once but only once since it cant edit the already edited df
-s1 = ez_sign_plot(df2, **parameters)  # Added pairs here
 
-plt.show()
+s1 = ez_sign_plot(df2,  **parameters)  # Added pairs here
+
+
 
 
