d92d6f20959cc284aada5093d43d4dde7ae6cb39
jay-cee-begs <john-carl.begley@charite.de>
2024-11-26 15:10:59 +0100

    Fix imports

--- src/plotting/plotting_constants.py
+++ src/plotting/plotting_constants.py
@@ -1,21 +1,21 @@
 import pandas as pd
-import batch_process.gui_configurations as gui_configurations
-from batch_process.gui_configurations import main_folder, parameters, TimePoints, exp_condition 
 #from functions_plots import *
 from statannotations.Annotator import Annotator
-from plotting.jd_plot_functions import ez_sign_plot, load_and_adjust
+from plotting import jd_plot_functions as stat_plot 
 import importlib # to reload the gui_configurations file
+from batch_process import gui_configurations as configurations 
 
 # Reload the gui_configurations module to ensure changes are picked up
-importlib.reload(gui_configurations)
-parameters = gui_configurations.parameters
-#print(f"Loaded parameters: {parameters}")  # Debugging statement
+importlib.reload(configurations)
+parameters = configurations.parameters
+#print(f"Loaded parameters: {parameter s}")  # Debugging statement
 
 
-df2 = load_and_adjust(TimePoints, exp_condition) #run once but only once since it cant edit the already edited df
+df2 = stat_plot.load_and_adjust(configurations.TimePoints, configurations.exp_condition) #run once but only once since it cant edit the already edited df
 
-s1 = ez_sign_plot(df2,  **parameters)  # Added pairs here
-
-
+s1 = stat_plot.ez_sign_plot(df2,  **parameters)  # Added pairs here
 
 
+
+
+ 
