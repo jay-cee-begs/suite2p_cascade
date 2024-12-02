71b3b2e08ec326185100b8d41c13bed354c95558
JDKleber <justus.donner@gmail.com>
2024-12-02 17:04:40 +0100

    Remove print of the current directory

--- src/batch_process/Scripts/run_plots.bat
+++ src/batch_process/Scripts/run_plots.bat
@@ -5,9 +5,9 @@ CALL "C:\Users\Justus\miniforge3\Scripts\activate.bat" data_env
 
 python -c "import sys; print('Python executable:', sys.executable); print('sys.path:', sys.path)"
 
-@REM Saving directory of batch file
+
 set script_dir=%~dp0
-echo script_dir is %script_dir% 
+
 set src_dir=%script_dir%..\..\
 
 
@@ -16,7 +16,7 @@ set src_dir = %script_dir%..\..\src
 
 cd "%src_dir%\plotting"
 
-echo current directory is %cd%
+
 
 :: Run the plotting script 
 python -m plotting_constants
