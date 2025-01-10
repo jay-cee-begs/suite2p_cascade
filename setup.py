e0a7e1c6a0ca547911119882cfe4d2c97be03234
jay-cee-begs <john-carl.begley@charite.de>
2025-01-10 12:11:47 +0100

    Fix setup.py
    Legacy version likely will need to be fixed in the future

--- setup.py
+++ setup.py
@@ -1,42 +1,4 @@
 from setuptools import setup, find_packages
-import yaml
-import chardet
-
-# Detect file encoding
-def detect_encoding(filename):
-    with open(filename, 'rb') as file:
-        raw_data = file.read()
-        result = chardet.detect(raw_data)
-        return result['encoding']
-
-def parse_requirements_yaml(filename, encoding):
-    with open(filename, 'r', encoding=encoding) as file:
-        data = yaml.safe_load(file)
-        return [str(line) for line in data.get('dependencies', [])]
-        # return [line.strip() for line in file if line.strip() and not line.startswith("#")]
-
-def parse_requirements_txt(filename, encoding):
-    """
-    Parses a plain text requirements file to extract dependencies.
-    """
-    with open(filename, 'r', encoding=encoding) as file:
-        return [
-            line.strip()
-            for line in file
-            if line.strip() and not line.startswith("#")  # Exclude empty lines and comments
-        ]
-
-
-# encoding = detect_encoding('requirements.txt')  # Or 'requirements.yaml'
-# print(f"Detected encoding: {encoding}")
-# requirements = parse_requirements_yaml('requirements.txt', encoding)
-env_requirements = {
-    'suite2p': parse_requirements_yaml('suite2p-req.yml', detect_encoding('suite2p-req.yml')),
-    'cascade': parse_requirements_yaml('cascade-req.yml', detect_encoding('cascade-req.yml')),
-    'data_env': parse_requirements_txt('statannotations-req.txt', detect_encoding('statannotations-req.txt'))
-
-
-}
 
 setup(
     name='suite2p_cascade', 
