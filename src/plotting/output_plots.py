84b732bc766df17a61f1b5d611652f9fbf8ebddf
jay-cee-begs <john-carl.begley@charite.de>
2025-02-05 12:48:25 +0100

    Update graph_outputs to use config.graph_settings

--- src/plotting/output_plots.py
+++ src/plotting/output_plots.py
@@ -6,18 +6,18 @@ config = load_json_config_file()
 def main():
     predictions_deltaF_files = functions_data_transformation.get_file_name_list(folder_path = config.general_settings.main_folder, file_ending = "predictions_deltaF.npy") ## get the names of the predicted spike files
     output_directories = functions_data_transformation.get_file_name_list(folder_path = config.general_settings.main_folder, file_ending = "samples")
-    
-    for file, output in zip(predictions_deltaF_files, output_directories):
-        fun_plot.histogram_total_estimated_spikes(file, output)
+    if config.graph_settings.total_estimated_spike_histogram:
+        for file, output in zip(predictions_deltaF_files, output_directories):
+            fun_plot.histogram_total_estimated_spikes(file, output)
     # # #TODO figure out how to compile group histograms
     # # # for group in groups:
     # # #     plot_group_histogram(group, predictions_deltaF_files)
     
-        spike_maximum = fun_plot.get_max_spike_across_frames(predictions_deltaF_files)
-
-    for file, output in zip(predictions_deltaF_files, output_directories):
-        fun_plot.plot_total_spikes_per_frame(file, spike_maximum, output)
-        fun_plot.plot_average_spike_probability_per_frame(file, output)
+    spike_maximum = fun_plot.get_max_spike_across_frames(predictions_deltaF_files)
+    if config.graph_settings.total_estimated_spikes_per_frame or config.graph_settings.avg_estimated_spikes_per_frame:
+        for file, output in zip(predictions_deltaF_files, output_directories):
+            fun_plot.plot_total_spikes_per_frame(file, spike_maximum, output)
+            fun_plot.plot_average_spike_probability_per_frame(file, output)
 
     functions_data_transformation.create_output_csv(config.general_settings.main_folder, overwrite = True, iscell_check=False, update_iscell=True)#overwrite = config.general_settings.overwrite, iscell_check = config.general_settings.iscell_check, update_iscell=config.general_settings.update_iscell)
     functions_data_transformation.csv_to_pickle(config.general_settings.main_folder, overwrite = True)
