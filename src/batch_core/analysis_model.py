c632fe00db31092920c5fd27ec6c33145629da46
jay-cee-begs <john-carl.begley@charite.de>
2026-05-13 16:40:27 +0200

    Update default settings for analysis_model.py

--- src/batch_core/analysis_model.py
+++ src/batch_core/analysis_model.py
@@ -7,20 +7,19 @@ class AnalysisParams:
     overwrite_cacade: bool = False
     multivid_processing: bool = False
     use_suite2p_ROI_classifier: bool = False
-    update_suite2p_iscell: bool = False
+    update_suite2p_iscell: bool = True
     Img_Overlay: str = "meanImg"
 
     cascade_activity_threshold: float = 0.1
     nb_neurons: int = 16
     model_name: str = "Global_EXC_10Hz_smoothing_200ms"
 
+    MAD_baseline_filter_threshold: float = 2.0
     baseline_correction: bool = False 
     correction_method: str = "airPLS"
+    lambda_window: int = 100
     normalize_peaks: bool = False
     normalization_method: str = 'Cell'
-
-    MAD_baseline_filter_threshold: float = 2.0
-    activity_threshold: float = 0.1
 
     def to_dict(self):
         return self.__dict__
