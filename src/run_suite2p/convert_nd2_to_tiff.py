c8457cde5a986c328e5d22fd55a958d470bbcc82
JC_home <jcbegs@gmail.com>
2026-05-22 11:54:21 +0200

    Create DocStrings for convert_nd2_to_tiff.py

--- src/run_suite2p/convert_nd2_to_tiff.py
+++ src/run_suite2p/convert_nd2_to_tiff.py
@@ -17,6 +17,22 @@ config = _DEFAULT_CONFIG
 
 
 def getFilesWithExt(top_dir, files_ext):
+    """
+    Function to get all files with given data extension (nd2) in a directory
+
+    Args:
+    ----------
+        top_dir : str / directory 
+            Top / Parent directory containing image files
+        files_ext: str
+            File extension for given image files (e.g., 'tiff', 'nd2', 'tif')
+            The function is optimized to find and convert ND2 files into tif files.
+    
+    Returns:
+    ----------
+        matches: list
+            list of files matching files_ext within top_dir 
+    """
     matches = []
     for root, dirnames, filenames in os.walk(str(top_dir)):
         for _dir in dirnames:
@@ -28,7 +44,24 @@ def getFilesWithExt(top_dir, files_ext):
     return matches
 
 def convertND2toTiff(fp_pathlib):
-    
+    """
+    Primary worker function to convert individual ND2 image file into tif file.
+    The function uses the tiffPathFromND2 to create each new tiff path.
+    The ND2 file is opened and re-saved as a tif file in a new subfolder with matching name.
+
+    Args:
+    ----------
+        fp_pathlib : str / Path-like Object 
+            File path leading to ND2 file to convert
+        
+    Returns:
+    ----------
+
+    Notes:
+    ----------
+        This function only works with a single image file. It is run, nested,
+        within the function iterConvert found below. 
+    """
     print("Attempting to convert:", str(fp_pathlib))
     save_fp = tiffPathFromND2(fp_pathlib)
     save_dir = save_fp.parent
@@ -43,9 +76,39 @@ def convertND2toTiff(fp_pathlib):
         print("Done converting")
         
 def tiffPathFromND2(_file):
+    """
+    Iterator function to create file paths for converted tif files into subfolders.
+    
+    Args:
+    ----------
+        _file : str / Path object 
+            File path leading to single tif file
+    
+    Returns:
+    ----------
+        Path(f"{_file.parent}/{_file.stem}/{_file.stem}.tif")
+            pathlib object creating a subfolder with the name of the file stem
+            _file.stem within the same directory leading to a .tif file of the same
+            name that is going to be created.
+    """
     return Path(f"{_file.parent}/{_file.stem}/{_file.stem}.tif")
 
 def iterConvert(config):
+    """
+    Wrapper function to automate converting all ND2 files into tif files
+    within a given directory.
+    The function uses 'getFilesWithExt' to get ND2 files, 
+    uses tiffPathFromND2 to create new file paths,
+    and uses 'convertND2toTiff' to create tif files from ND2 files
+    Args:
+    ----------
+        config : SimpleNameSpace dictionary 
+            config.json file loaded as SimpleNameSpace dictionary
+    
+    Returns:
+    ----------
+        None
+    """
     BASE_DIR = config.general_settings.main_folder
 
     tiff_files = getFilesWithExt(BASE_DIR, ".tif")
